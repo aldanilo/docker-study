08022d3a4d3b91b0cd3d9983ef044f11daaecaca
aldanilo <30423596+aldanilo@users.noreply.github.com>
2019-06-04 23:51:22 -0300

    Update init.sql

--- docker-tutorial-master/docker-tutorial-master/db/init.sql
+++ docker-tutorial-master/docker-tutorial-master/db/init.sql
@@ -1,18 +1,18 @@
 CREATE DATABASE geofusion;
 use geofusion;
 
+
 CREATE TABLE bairros (
-  codigo VARCHAR(255) NOT NULL
-  , nome VARCHAR(255) NOT NULL
-  , municipio VARCHAR(255) NOT NULL
-  , uf VARCHAR(255) NOT NULL
+  codigo VARCHAR(255) CHARACTER SET UTF8 COLLATE utf8_general_ci
+  , nome VARCHAR(255) CHARACTER SET UTF8 COLLATE utf8_general_ci
+  , municipio VARCHAR(255) CHARACTER SET UTF8 COLLATE utf8_general_ci
+  , uf VARCHAR(255) CHARACTER SET UTF8 COLLATE utf8_general_ci
   , area FLOAT NOT NULL
 );
 
 LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/bairros.csv' 
 INTO TABLE bairros 
-CHARACTER SET UTF8 
+CHARACTER SET UTF8
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS;
-

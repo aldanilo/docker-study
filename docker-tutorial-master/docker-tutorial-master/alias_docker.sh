eabeef01b0b552a8e54ce30a23d962150d5f4e15
aldanilo <30423596+aldanilo@users.noreply.github.com>
2019-06-04 23:16:58 -0300

    Create alias_docker.sh

---
+++ docker-tutorial-master/docker-tutorial-master/alias_docker.sh
@@ -0,0 +1,7 @@
+echo alias docker-compose="'"'docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.24.0'"'" >> ~/.bashrc
+
+source ~/.bashrc

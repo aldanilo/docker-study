670304917fa925c9315d8e783a5460723447ea28
aldanilo <30423596+aldanilo@users.noreply.github.com>
2019-06-04 23:07:13 -0300

    Update init.sql

--- docker-tutorial-master/docker-tutorial-master/db/init.sql
+++ docker-tutorial-master/docker-tutorial-master/db/init.sql
@@ -1,13 +1,18 @@
 CREATE DATABASE geofusion;
 use geofusion;
 
-CREATE TABLE favorite_colors (
-  name VARCHAR(20),
-  color VARCHAR(10)
+CREATE TABLE bairros (
+  codigo VARCHAR(255) NOT NULL
+  , nome VARCHAR(255) NOT NULL
+  , municipio VARCHAR(255) NOT NULL
+  , uf VARCHAR(255) NOT NULL
+  , area FLOAT NOT NULL
 );
 
-INSERT INTO favorite_colors
-  (name, color)
-VALUES
-  ('Lancelot', 'blue'),
-  ('Galahad', 'yellow');
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/bairros.csv' 
+INTO TABLE bairros 
+CHARACTER SET UTF8 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS;
+
